ed7c711b452c59f82a71a9e7a36c017641c0e742
Youcef Aouali <you.aouali@gmail.com>
2021-03-28 17:06:46 +0200

    listing classes du controller

---
+++ PROJET/MVC/controller/__init__.py
@@ -0,0 +1,17 @@
+class HomeMenuController:
+    pass
+
+class AddTournamentController:
+    pass
+
+class AddPlayerController:
+    pass
+
+class MakeMatchmakingController:
+    pass
+
+class AddScoreController:
+    pass
+    
+class MakeReportController:
+    pass
