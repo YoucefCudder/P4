d3fe4dc6c7b32a0b86163fc75b40ec42e507ae88
Youcef Aouali <you.aouali@gmail.com>
2021-03-29 20:40:07 +0200

    essai de serialiser les joueurs (à revoir)

--- PROJET/MVC/model/modele.py
+++ PROJET/MVC/model/modele.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # coding: utf8
-
+from tinydb import TinyDB
 # recherche des objets contenant toute la logique de mon logiciel de gestion de tournois d'échecs
 # les classes de mon modèle doivent permettre de rendre le logiciel au minimum fonctionnel sans vue(interface)
 # et sans controller.
@@ -76,10 +76,73 @@ class Player:
         self.score += 0.5
         pass
 
-    def losing(self):
-        """Docstring"""
-        pass
+player_1 = Player(name='Ngannou', f_name='Francis', age=34, birthdate=None, gender='M', ranking=None, score=0)
+player_2 = Player(name='Nurmagomedov', f_name='Khabib', age=32, birthdate=None, gender='M', ranking=None, score=0)
+player_3 = Player(name='Nunes', f_name='Amanda', age=31, birthdate=None, gender='F', ranking=None, score=0)
+player_4 = Player(name='Jones', f_name='Jon', age=35,  birthdate=None, gender='M', ranking=None, score=0)
+player_5 = Player(name='Adesanya', f_name='Israel', age=30, birthdate=None, gender='M', ranking=None, score=0)
+player_6 = Player(name='Zhang', f_name='Weili', age=29, birthdate=None, gender='F', ranking=None, score=0)
+player_7 = Player(name='Yan', f_name='Petr', age=28, birthdate=None, gender='M', ranking=None, score=0)
+player_8 = Player(name='St-Pierre', f_name='Georges', age=39, birthdate=None, gender='M', ranking=None, score=0)
 
+
+serialized_player_1 = {
+    'name': player_1.name,
+    'f_name' : player_1.f_name, 
+    'age': player_1.age,
+    'birthdate' : player_1.birthdate,
+    'gender' : player_1.gender, 
+    'ranking' : player_1.ranking,
+    'score' : player_1.score
+    
+}
+serialized_player_2 = {
+    'name': player_1.name,
+    'f_name' : player_2.f_name, 
+    'age': player_2.age,
+    'birthdate' : player_2.birthdate,
+    'gender' : player_2.gender, 
+    'ranking' : player_2.ranking,
+    'score' : player_2.score
+    
+}
+serialized_player_3 = {
+    'name': player_3.name,
+    'f_name' : player_3.f_name, 
+    'age': player_3.age,
+    'birthdate' : player_3.birthdate,
+    'gender' : player_3.gender, 
+    'ranking' : player_3.ranking,
+    'score' : player_3.score
+    
+}
+serialized_player_4 = {
+    'name': player_4.name,
+    'f_name' : player_4.f_name, 
+    'age': player_4.age,
+    'birthdate' : player_4.birthdate,
+    'gender' : player_4.gender, 
+    'ranking' : player_4.ranking,
+    'score' : player_4.score
+    
+}
+serialized_players = (serialized_player_1, serialized_player_2, serialized_player_3, serialized_player_4)
+
+def __index__(self): 
+    name = serialized_players['name']
+    f_name = serialized_players['f_name']
+    age = serialized_players['age']
+    birthdate = serialized_players['birthdate']
+    gender = serialized_players['gender']
+    ranking = serialized_players['ranking']
+    score = serialized_players['score']
+
+db = TinyDB('db.json')
+players_table = db.table('players')
+players_table.truncate()	# clear the table first
+players_table.insert_multiple(serialized_players)
+
+serialized_players = players_table.all()
 
 class Players:
     """class to manage a list of players for a tournament"""
